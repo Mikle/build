3f6af826abf0052d4e155b7a49da6c70ec275f4b
Mikle <mikle@mikle.com>
2017-05-19 10:38:14 +0300

    Revert "Merge branch 'Atla'"
    This reverts commit 5764b54ddc2f4dee3e72fc53df274c8ae8ebe18c, reversing
    changes made to a51bd5c2945783ffe14dd5dbe78236c483fc8eb1.

--- js/common.js
+++ js/common.js
@@ -1,46 +1,12 @@
 	//Chrome Smooth Scroll
-$(document).ready(function(){
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
 
-        var $menu = $("#menu");
 
-        $(window).scroll(function(){
-            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
-                $menu.removeClass("default").addClass("fixed");
-            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
-                $menu.removeClass("fixed").addClass("default");
-            }
-        });//scroll
-    });
+	};
 
-	$(document).ready(function(){
-
-        var $menu = $("#menu");
-
-        $(window).scroll(function(){
-            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
-                $menu.fadeOut('fast',function(){
-                    $(this).removeClass("default")
-                           .addClass("fixed transbg")
-                           .fadeIn('fast');
-                });
-            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
-                $menu.fadeOut('fast',function(){
-                    $(this).removeClass("fixed transbg")
-                           .addClass("default")
-                           .fadeIn('fast');
-                });
-            }
-        });//scroll
-
-        $menu.hover(
-            function(){
-                if( $(this).hasClass('fixed') ){
-                    $(this).removeClass('transbg');
-                }
-            },
-            function(){
-                if( $(this).hasClass('fixed') ){
-                    $(this).addClass('transbg');
-                }
-            });//hover
-    });//jQuery
+	
